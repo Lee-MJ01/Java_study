a0fc168a69048d481e924dbf17e6323221490b6b
alswnstl23 <alswnstl23@gmail.com>
2025-07-11 11:24:16 +0900

    add SampleProject

---
+++ SampleProject/src/p96/MultipleOfThree.java
@@ -0,0 +1,17 @@
+package p96;
+
+import java.util.Scanner;
+
+public class MultipleOfThree {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("수를 입력하세요 : ");
+		int number = sc.nextInt();
+		if(number % 3 == 0)
+			System.out.println("3의 배수입니다.");
+
+	}
+
+}
